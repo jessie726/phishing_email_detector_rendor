50c9034f82b83dd8e12a5bb83bcecb7a5c68c455
JIN YAN <jinyan@JINs-MacBook-Air.local>
2025-09-04 19:09:11 -0700

    add new files

--- etl/load.py
+++ etl/load.py
@@ -5,19 +5,17 @@ from tensorflow.keras.models import load_model
 
 
 def load_pipeline(
-    model_path="final_models/final_bert.h5",
+    model_path="final_models/final_lstm.h5",
     tokenizer_path="final_models/tokenizer.pkl",
     drive_id=None,
     maxlen=120
 ):
 
     # Download model from Google Drive if not found
-
     if not os.path.exists(model_path) and drive_id:
         url = f"https://drive.google.com/uc?id={drive_id}"
         print(f"Model not found locally. Downloading from {url} ...")
         gdown.download(url, model_path, quiet=False)
-
 
     # Load model
     model = load_model(model_path, compile=False)
