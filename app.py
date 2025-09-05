d25424c47fa70ed257ae7e955a119217059c923a
JIN YAN <jinyan@JINs-MacBook-Air.local>
2025-09-04 19:27:31 -0700

    add new files

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, render_template, jsonify
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle, os
 from etl.load import load_pipeline
 
 model, tokenizer, MAXLEN = load_pipeline(
-    model_path="final_models/final_bert.pkl",
+    model_path="final_models/final_tfidf.pkl",
     tokenizer_path="final_models/tokenizer.pkl",
     drive_id="1G0VwRzWS2LmRfjSkkts8gbj902XJuA36",  # your Google Drive file ID
     maxlen=120
@@ -22,13 +21,15 @@ def home():
     if request.method == "POST":
         email_text = request.form.get("email_text", "")
 
-        # Transform input
-        seq = tokenizer.texts_to_sequences([email_text])
-        padded = pad_sequences(seq, maxlen=MAXLEN)
+        # Transform input using TF-IDF
+        X = tokenizer.transform([email_text])
 
         # Predict
-        pred = model.predict(padded)[0][0]
-        prediction = f"Phishing ({pred:.2%})" if pred > 0.5 else f"Legitimate ({1-pred:.2%})"
+        pred = model.predict_proba(X)[0]
+        phishing_prob = pred[1]
+        legit_prob = pred[0]
+
+        prediction = f"Phishing ({phishing_prob:.2%})" if phishing_prob > 0.5 else f"Legitimate ({legit_prob:.2%})"
 
     return render_template("index.html", prediction=prediction, email_text=email_text)
 
